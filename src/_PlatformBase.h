8e127e9fe9355c382b4f3bf8a8fa5160208dabfb
Shinovon <arman200711@gmail.com>
2025-05-12 15:11:43 +0500

    Reformat indentations

--- src/_PlatformBase.h
+++ src/_PlatformBase.h
@@ -196,13 +196,13 @@ static void EncipherBlock(cc_uint32* v, const cc_uint32* key, cc_string* dst) {
 	cc_uint32 v0 = v[0], v1 = v[1], sum = 0, delta = 0x9E3779B9;
 	int i;
 
-    for (i = 0; i < 12; i++) 
+	for (i = 0; i < 12; i++) 
 	{
-        v0  += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-        sum += delta;
-        v1  += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
-    }
-    v[0] = v0; v[1] = v1;
+		v0  += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+		sum += delta;
+		v1  += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+	}
+	v[0] = v0; v[1] = v1;
 	String_AppendAll(dst, v, 8);
 }
 
@@ -210,13 +210,13 @@ static void DecipherBlock(cc_uint32* v, const cc_uint32* key) {
 	cc_uint32 v0 = v[0], v1 = v[1], delta = 0x9E3779B9, sum = delta * 12;
 	int i;
 
-    for (i = 0; i < 12; i++) 
+	for (i = 0; i < 12; i++) 
 	{
-        v1  -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
-        sum -= delta;
-        v0  -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-    }
-    v[0] = v0; v[1] = v1;
+		v1  -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+		sum -= delta;
+		v0  -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+	}
+	v[0] = v0; v[1] = v1;
 }
 
 #define ENC1 0xCC005EC0
